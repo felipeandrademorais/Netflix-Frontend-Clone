ebaa1858e5cf5e8198b02ee0f5225a751b974cd0
felipe.morais <felipe.morais@sc.senai.br>
2021-07-23 09:49:43 -0300

    Configurado api para buscar filmes e criado .env_exemplo

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import Tmdb from "./services/Api";
+
+type List = {
+  slug: string,
+  title: string,
+  items: []
+}
 
 function App() {
+
+  const [movieList, setMovieList] = useState<List[]>([]);
+
+  useEffect(() => {  
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();  
+      setMovieList(list);
+    }
+    loadAll();
+  }, []);
+
   return (
-    <h1>Hello Netflix Clone</h1>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item)=>(
+          <div>{item.title}</div>
+        ))}
+      </section>
+    </div>
   );
 }
 
